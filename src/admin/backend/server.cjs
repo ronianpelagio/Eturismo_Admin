870892aa9afcbc25e9ddf43700600a6e3c5c871c
ronianpelagio <ronianpelagio111@gmail.com>
2026-06-07 13:42:14 +0800

    server modify

--- src/admin/backend/server.cjs
+++ src/admin/backend/server.cjs
@@ -17,14 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
-const cors = require('cors');
 
-app.use(cors({
-  origin: [
-    'https://eturismo-admin.vercel.app/'
-  ],
-  credentials: true
-}));
 // Language mapping for gSpeak
 const LANG_MAP = {
   en: 'en',
